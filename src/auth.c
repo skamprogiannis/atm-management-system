f071639b92698edb7901b5cc05b3cdbb557faa7e
skamprogiannis <boot.stefan.os@proton.me>
2026-06-08 17:25:14 +0300

    style: format auth and menu code

--- src/auth.c
+++ src/auth.c
@@ -1,59 +1,55 @@
-#include <termios.h>
 #include "header.h"
+#include <termios.h>
 
 char *USERS = "./data/users.txt";
 
-void loginMenu(char a[50], char pass[50])
-{
-    struct termios oflags, nflags;
+void registerMenu(char a[50], char pass[50]) {}
 
-    system("clear");
-    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
-    scanf("%49s", a);
+void loginMenu(char a[50], char pass[50]) {
+  struct termios oflags, nflags;
 
-    // disabling echo
-    tcgetattr(fileno(stdin), &oflags);
-    nflags = oflags;
-    nflags.c_lflag &= ~ECHO;
-    nflags.c_lflag |= ECHONL;
+  system("clear");
+  printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+  scanf("%49s", a);
 
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
-    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
-    scanf("%s", pass);
+  // disabling echo
+  tcgetattr(fileno(stdin), &oflags);
+  nflags = oflags;
+  nflags.c_lflag &= ~ECHO;
+  nflags.c_lflag |= ECHONL;
 
-    // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
+  if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
+    perror("tcsetattr");
+    return exit(1);
+  }
+  printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+  scanf("%s", pass);
+
+  // restore terminal
+  if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0) {
+    perror("tcsetattr");
+    return exit(1);
+  }
 };
 
-const char *getPassword(struct User u)
-{
-    FILE *fp;
-    struct User userChecker;
+const char *getPassword(struct User u) {
+  FILE *fp;
+  struct User userChecker;
 
-    if ((fp = fopen("./data/users.txt", "r")) == NULL)
-    {
-        printf("Error! opening file");
-        exit(1);
+  if ((fp = fopen("./data/users.txt", "r")) == NULL) {
+    printf("Error! opening file");
+    exit(1);
+  }
+
+  while (fscanf(fp, "%49s %49s", userChecker.name, userChecker.password) !=
+         EOF) {
+    if (strcmp(userChecker.name, u.name) == 0) {
+      fclose(fp);
+      char *buff = userChecker.password;
+      return buff;
     }
+  }
 
-    while (fscanf(fp, "%49s %49s", userChecker.name, userChecker.password) != EOF)
-    {
-        if (strcmp(userChecker.name, u.name) == 0)
-        {
-            fclose(fp);
-            char *buff = userChecker.password;
-            return buff;
-        }
-    }
-
-    fclose(fp);
-    return "no user found";
+  fclose(fp);
+  return "no user found";
 }
