d772bac3d76e76d6abe247bf8f1ecd37e417c376
skamprogiannis <boot.stefan.os@proton.me>
2026-06-09 19:07:43 +0300

    fix(records): correct account file IO

--- src/system.c
+++ src/system.c
@@ -3,16 +3,16 @@
 const char *RECORDS = "./data/records.txt";
 
 int getAccountFromFile(FILE *ptr, char name[50], struct Record *r) {
-  return fscanf(ptr, "%d %d %s %d %d/%d/%d %s %d %lf %s", &r->id, &r->userId,
-                name, &r->accountNbr, &r->deposit.month, &r->deposit.day,
-                &r->deposit.year, r->country, &r->phone, &r->amount,
-                r->accountType) != EOF;
+  return fscanf(ptr, "%d %d %49s %d %d/%d/%d %99s %d %lf %9s", &r->id,
+                &r->userId, name, &r->accountNbr, &r->deposit.month,
+                &r->deposit.day, &r->deposit.year, r->country, &r->phone,
+                &r->amount, r->accountType) == 11;
 }
 
 void saveAccountToFile(FILE *ptr, struct User u, struct Record r) {
-  fprintf(ptr, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n\n", &r->id,
-          &u->id & u->name, r.accountNbr, r.deposit.month, r.deposit.day,
-          r.deposit.year, r.country, r.phone, r.amount, r.accountType);
+  fprintf(ptr, "%d %d %49s %d %d/%d/%d %99s %d %.2lf %9s\n\n", r.id, u.id,
+          u.name, r.accountNbr, r.deposit.month, r.deposit.day, r.deposit.year,
+          r.country, r.phone, r.amount, r.accountType);
 }
 
 void stayOrReturn(int notGood, void f(struct User u), struct User u) {
